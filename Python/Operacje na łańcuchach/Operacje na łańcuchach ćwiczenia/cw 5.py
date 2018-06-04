7e0f41fd4c2be696ceff60f7be4ab2eeb226c07f
Karolina <kontakt@karolinakieltyka.pl>
2018-06-04 10:04:53 +0200

    ćwiczenia cd

---
+++ Python/Operacje na łańcuchach/Operacje na łańcuchach ćwiczenia/cw 5.py
@@ -0,0 +1,6 @@
+"""
+tworzenie łańcuchów za pomocą konkatenacji i za pomocą powielania
+"""
+
+print("Sherlock "+ "Holmes")
+print("B" * 2 +"C")
---
+++ Python/Operacje na łańcuchach/Operacje na łańcuchach ćwiczenia/cw 6.py
@@ -0,0 +1,8 @@
+"""
+zwróć łańcuch znaków do pierwszej kropki
+"""
+quote = "Powiedziałeś mi kiedyś, że nie jesteś bohaterem. Czasami wątpiłem, czy w ogóle jesteś człowiekiem… "
+#print(quote.index("."))
+
+print(quote[0:47])
+
